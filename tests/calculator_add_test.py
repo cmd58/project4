4b68f39ff8ab721f345df6744218de33eafcc005
cmd58 <cmd58@njit.edu>
2022-05-12 16:42:47 -0400

    test click

--- tests/calculator_add_test.py
+++ tests/calculator_add_test.py
@@ -3,7 +3,7 @@ from calculator import Calculator
 
 
 def test_calculator_add_method():
-    """Testing the Calculator"""
+    """Testing the Calculator Add"""
     calculator = Calculator()
     assert calculator.add(1) == 1
 
---
+++ tests/click_test.py
@@ -0,0 +1,6 @@
+"""Tests the clicks"""
+
+
+from click.testing import CliRunner
+
+runner = CliRunner()
